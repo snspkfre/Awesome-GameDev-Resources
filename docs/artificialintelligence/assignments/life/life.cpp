c8978193c8897c81a0b04023bfe97a018a0493f7
Victor <victor.diab@mymail.champain.edu>
2024-09-14 12:54:12 -0400

    Finished game of life and passes all tests. Changed array to 2d array since accessing wasn't working.

--- docs/artificialintelligence/assignments/life/life.cpp
+++ docs/artificialintelligence/assignments/life/life.cpp
@@ -1,18 +1,22 @@
-#include <iomanip>;
+#include <iomanip>
 #include <iostream>
-#include <string>;
+#include <string>
 
 using namespace std;
 
-int countNeighbors(bool* arr, int x, int y, int sizeX, int sizeY);
+int countNeighbors(bool** arr, int x, int y, int sizeX, int sizeY);
 
 int main()
 {
   int x, y, s;
   cin >> x >> y >> s;
   cin.ignore();
-  bool* arr;
-  arr = new bool[y*x];
+  bool** arr;
+  arr = new bool*[y];
+  for(int i = 0; i < y; i++)
+  {
+    arr[i] = new bool[x];
+  }
   
   // access by showing arr[x_value * x + y]
   // arr[9 * x + 10] = 87; for y = 9, x = 10
@@ -23,32 +27,38 @@ int main()
       const char* input = str.c_str();
       for (int j = 0; j < x; j++)
       {
-        arr[i * x + j] = input[j] == '#';
+        arr[i][j] = input[j] == '#';
       }
   }
 
   for (int k = 0; k < s; k++)
   {
-    bool* next;
-    next = new bool[y * x];
+    bool** next;
+    next = new bool*[y];
+    for(int i = 0; i < y; i++)
+    {
+      next[i] = new bool[x];
+    }
+
     for (int i = 0; i < y; i++) {
       for (int j = 0; j < x; j++) {
-        if (!arr[i * x + j])
+        if (!arr[i][j])
         {
-          next[i * x + j] = countNeighbors(arr, i, j, x, y) == 3;
+          next[i][j] = countNeighbors(arr, j, i, x, y) == 3;
         }
         else
         {
-          if (countNeighbors(arr, i, j, x, y) < 2 || countNeighbors(arr, i, j, x, y) > 3)
-            next[i * x + j] = false;
+          if (countNeighbors(arr, j, i, x, y) < 2 || countNeighbors(arr, j, i, x, y) > 3)
+            next[i][j] = false;
           else
-            next[i * x + j] = true;
+            next[i][j] = true;
         }
       }
     }
 
     arr = next;
     next = nullptr;
+    delete[] next;
   }
   //system("cls");
 
@@ -56,27 +66,38 @@ int main()
   {
       for (int j = 0; j < x; j++)
       {
-          char out = arr[i * x + j] ? '#' : '.';
-          cout << out;
+        //testing thing
+        //countNeighbors(arr, i, j, x, y);
+          //cout << countNeighbors(arr, i, j, x, y) << " ";
+          char outValue = arr[i][j] ? '#' : '.';
+          cout << outValue;
       }
       cout << "\n";
   }
+  arr = nullptr;
+  delete[] arr;
 }
 
-int countNeighbors(bool* arr, int x, int y, int sizeX, int sizeY) {
-    int down = (y - 1) < 0 ? y-1 + sizeY : (y - 1);
-    int up = (y + 1) % sizeY;
-    int left = (x - 1) < 0 ? x - 1 + sizeX : (x - 1);
-    int right = (x + 1) % sizeX;
+int countNeighbors(bool** arr, int x, int y, int sizeX, int sizeY) {
+  int down, up, left, right;
 
-    int sum = 0;
-    sum += arr[left * sizeX + up];
-    sum += arr[left * sizeX + y];
-    sum += arr[right * sizeX + y];
-    sum += arr[right * sizeX + up];
-    sum += arr[x * sizeX + up];
-    sum += arr[left * sizeX + down];
-    sum += arr[x * sizeX + down];
-    sum += arr[right * sizeX + down];
-    return sum;
+  if(y == 0) down = sizeY - 1;
+  else down = y - 1;
+  if(y == sizeY - 1) up = 0;
+  else up = y + 1;
+  if(x == 0) left = sizeX - 1;
+  else left = x - 1;
+  if(x == sizeX - 1) right = 0;
+  else right = x + 1;
+
+  int sum = 0;
+  sum += arr[up][left] ? 1 : 0;
+  sum += arr[up][x] ? 1 : 0;
+  sum += arr[up][right] ? 1 : 0;
+  sum += arr[y][left] ? 1 : 0;
+  sum += arr[y][right] ? 1 : 0;
+  sum += arr[down][left] ? 1 : 0;
+  sum += arr[down][x] ? 1 : 0;
+  sum += arr[down][right] ? 1 : 0;
+  return sum;
 }
